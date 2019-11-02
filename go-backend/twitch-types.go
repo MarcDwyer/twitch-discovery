dd0a658e261aef66fee58cc6ebf0a67d03612c35
Marc Dwyer <marcdwyer1738@gmail.com>
2019-11-01 20:37:02 -0500

    some minor styling changes

--- go-backend/twitch-types.go
+++ go-backend/twitch-types.go
@@ -1,55 +1,8 @@
 package main
 
 import (
-	"time"
-
 	"github.com/MarcDwyer/twitchgo"
 )
-
-type TResponse struct {
-	Total   int        `json:"_total"`
-	Streams []TStreams `json:"streams"`
-}
-type SingleResponse struct {
-	Stream *TStreams `json:"stream"`
-}
-type TStreams struct {
-	ID         int64     `json:"_id"`
-	AverageFps float64   `json:"average_fps"`
-	Channel    TChannel  `json:"channel"`
-	CreatedAt  time.Time `json:"created_at"`
-	Delay      int       `json:"delay"`
-	Game       string    `json:"game"`
-	IsPlaylist bool      `json:"is_playlist"`
-	Preview    struct {
-		Large    string `json:"large"`
-		Medium   string `json:"medium"`
-		Small    string `json:"small"`
-		Template string `json:"template"`
-	} `json:"preview"`
-	VideoHeight int `json:"video_height"`
-	Viewers     int `json:"viewers"`
-}
-type TChannel struct {
-	ID                           int         `json:"_id"`
-	BroadcasterLanguage          string      `json:"broadcaster_language"`
-	CreatedAt                    time.Time   `json:"created_at"`
-	DisplayName                  string      `json:"display_name"`
-	Followers                    int         `json:"followers"`
-	Game                         string      `json:"game"`
-	Language                     string      `json:"language"`
-	Logo                         string      `json:"logo"`
-	Mature                       bool        `json:"mature"`
-	Name                         string      `json:"name"`
-	Partner                      bool        `json:"partner"`
-	ProfileBanner                string      `json:"profile_banner"`
-	ProfileBannerBackgroundColor interface{} `json:"profile_banner_background_color"`
-	Status                       string      `json:"status"`
-	UpdatedAt                    time.Time   `json:"updated_at"`
-	URL                          string      `json:"url"`
-	VideoBanner                  string      `json:"video_banner"`
-	Views                        int         `json:"views"`
-}
 
 type Stream struct {
 	Stream      *twitchgo.TStreams `json:"streamData"`
--- node-backend/src/main.ts
+++ node-backend/src/main.ts
@@ -1,33 +1,32 @@
-import express from 'express'
-import http from 'http'
-import ioSetup from 'socket.io'
-import dotenv from 'dotenv'
-import TwitchDiscovery, { TwitchDisc } from './twitch_discovery'
-import { handleOffset } from './offset-change'
+import express from "express";
+import http from "http";
+import ioSetup from "socket.io";
+import dotenv from "dotenv";
+import TwitchDiscovery, { TwitchDisc } from "./twitch_discovery";
+import { handleOffset } from "./offset-change";
 
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import bodyParser from "body-parser";
+import cors from "cors";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express(),
-    port = 5005,
-    server = new http.Server(app),
-    io = ioSetup(server);
+  port = 5005,
+  server = new http.Server(app),
+  io = ioSetup(server);
 
 app.use(bodyParser.json());
 app.use(cors());
 
 (async () => {
-    const streams: TwitchDisc = new TwitchDiscovery(io)
-    await streams.populateRandom()
+  const streams: TwitchDisc = new TwitchDiscovery(io);
+  await streams.populateRandom();
 
-    io.on('connection', (socket) => socket.emit('init-data', streams.data))
+  io.on("connection", socket => socket.emit("init-data", streams.data));
 
-    app.use('/test', (req, res) => res.send(JSON.stringify(streams.data)))
+  app.use("/test", (req, res) => res.send(JSON.stringify(streams.data)));
 
-    app.post('/set-offset/', async (req, res) => handleOffset(req, res, streams))
+  app.post("/set-offset/", async (req, res) => handleOffset(req, res, streams));
 
-    server.listen(port)
-})()
+  server.listen(port);
+})();
